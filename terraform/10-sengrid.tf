548bd56e1fa8f2f471ee45826ceb2507cdfa413d
James Johnson <jrjohnson87@googlemail.com>
2020-10-04 15:41:47 +0100

    Update

--- terraform/10-sengrid.tf
+++ terraform/10-sengrid.tf
@@ -64,7 +64,7 @@ resource "azurerm_key_vault" "keyvault" {
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+    object_id = data.azurerm_client_config.current.service_principal_object_id
 
     key_permissions = [
       "get", "create", "list"
