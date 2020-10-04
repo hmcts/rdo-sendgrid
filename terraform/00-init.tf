2c851ef4b861937f666a78522883dea7e5a99c54
James Johnson <jrjohnson87@googlemail.com>
2020-10-04 13:04:30 +0100

    Update

--- terraform/00-init.tf
+++ terraform/00-init.tf
@@ -1,7 +1,6 @@
 terraform {
   required_version = ">= 0.12.25"
   backend "azurerm" {
-    subscription_id = "2b1afc19-5ca9-4796-a56f-574a58670244"
   }
   required_providers {
     azurerm = "~> 2.17.0"
